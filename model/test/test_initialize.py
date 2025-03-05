2b106f6b6cdac135e64a4fae5b859a1774fe3bc4
yynil <yueyulin@me.com>
2025-03-05 15:04:38 +0000

    add example

--- model/test/test_initialize.py
+++ model/test/test_initialize.py
@@ -1,18 +1,37 @@
 if __name__ == '__main__':
     from cosyvoice.cli.cosyvoice import CosyVoice2
-    model_path = '/external_data/models/CosyVoice2-0.5B_RWKV_0.19B/'
+    import torch
+    model_path = '/home/yueyulin/models/CosyVoice2-0.5B_RWKV_0.19B/'
     device = 'cuda:0'
-    cosyvoice = CosyVoice2(model_path,device=device,fp16=True)
+    cosyvoice = CosyVoice2(model_path,device=device,fp16=True,load_jit=True)
     
     from cosyvoice.utils.file_utils import load_wav
     import torchaudio
-    prompt_audio_file = '/home/yueyulin/github/RWKVTTS/another.wav'
     prompt_audio_file = '/home/yueyulin/github/RWKVTTS/zero_shot_prompt.wav'
     prompt_audio_file = '/home/yueyulin/github/RWKVTTS/mine.wav'
+    prompt_audio_file = '/home/yueyulin/github/RWKVTTS/another.wav'
     prompt_speech_16k = load_wav(prompt_audio_file, 16000)
-    prompt_text = '我家里有三只狗。'
     prompt_text = '希望你以后做的比我还好呦。'
     prompt_text = '今天天气挺不错的。'
+    prompt_text = '我家里有三只狗。'
     cosyvoice.model.llm.dummy_forward()
-    for i, j in enumerate(cosyvoice.inference_zero_shot('收到好友从远方寄来的生日礼物，那份意外的惊喜与深深的祝福让我心中充满了甜蜜的快乐，笑容如花儿般绽放。', prompt_text, prompt_speech_16k, stream=False)):
-        torchaudio.save('zero_shot_{}.wav'.format(i), j['tts_speech'], cosyvoice.sample_rate)
+    print('Finished warmup')
+    prompt_audios = [
+        '/home/yueyulin/github/RWKVTTS/zero_shot_prompt.wav',
+        '/home/yueyulin/github/RWKVTTS/mine.wav',
+        '/home/yueyulin/github/RWKVTTS/another.wav'
+    ]
+    prompt_texts = [
+        '希望你以后做的比我还好呦。',
+        '今天天气挺不错的。',
+        '我家里有三只狗。'
+    ]
+    import logging
+    tts_text = '中国在东亚，是世界上最大的国家，也是世界上人口最多的国家。'
+    for i, (prompt_audio_file, prompt_text) in enumerate(zip(prompt_audios, prompt_texts)):
+        logging.info(f'Processing {prompt_text}')
+        prompt_speech_16k = load_wav(prompt_audio_file, 16000)
+        with torch.no_grad():
+            for j, k in enumerate(cosyvoice.inference_zero_shot(tts_text,prompt_text, prompt_speech_16k, stream=False,speed=1.)):
+                torchaudio.save('zero_{}_{}.wav'.format(i, j), k['tts_speech'], cosyvoice.sample_rate)
+        logging.info(f'Finished processing {prompt_text}')
