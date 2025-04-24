db04aaf51f89d013ec211e2d537c340c6d32e055
yueyulin <yueyu.lin@me.com>
2025-04-24 08:39:24 +0800

    添加依赖文件requirements.txt，并更新CosyVoice模型加载逻辑以提供加载信息。

--- third_party/cosyvoice/cli/model.py
+++ third_party/cosyvoice/cli/model.py
@@ -67,7 +67,8 @@ class CosyVoiceModel:
         self.hift_cache_dict = {}
 
     def load(self, llm_model, flow_model, hift_model):
-        self.llm.load_state_dict(torch.load(llm_model, map_location=self.device), strict=True)
+        info = self.llm.load_state_dict(torch.load(llm_model, map_location=self.device), strict=False)
+        print(f'load llm model {llm_model} with info: {info}')
         self.llm.to(self.device).eval()
         self.flow.load_state_dict(torch.load(flow_model, map_location=self.device), strict=True)
         self.flow.to(self.device).eval()
