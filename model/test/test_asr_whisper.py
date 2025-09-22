de56d349c31f24233b3c3fc82f9f436d6ad3ec65
yynil <yueyu.lin@me.com>
2025-09-22 08:07:25 +0800

    在ASR Whisper测试脚本中引入Whisper特征提取器和编码器，优化音频模型的加载过程，增加调试信息以便于跟踪模型状态和配置。

--- model/test/test_asr_whisper.py
+++ model/test/test_asr_whisper.py
@@ -1,10 +1,20 @@
 import click
 import torch
-from model.llm.rwkv_asr_whisper import RWKV7ASRModel,load_whisper_feature_extractor_and_encoder,RWKV7ModelForLatentInputs
+from model.llm.rwkv_asr_whisper import RWKV7ASRModel,RWKV7ModelForLatentInputs
 from rwkvfla.models.rwkv7.modeling_rwkv7 import RWKV7Config, RWKV7Model, RWKV7ForCausalLM
 from tokenizer.rwkv_tokenizer import RWKV_TOKENIZER
-from transformers import AutoTokenizer
+import transformers
+from transformers import AutoTokenizer,WhisperFeatureExtractor,WhisperConfig
+from transformers.models.whisper.modeling_whisper import WhisperEncoder
 import soundfile as sf
+def load_whisper_feature_extractor_and_encoder(whisper_path):
+    feature_extractor = WhisperFeatureExtractor.from_pretrained(whisper_path)
+    print(f"Loaded WhisperFeatureExtractor: {feature_extractor}")
+    whisper_config = WhisperConfig.from_pretrained(whisper_path)
+    print(f"Loaded WhisperConfig: {whisper_config}")
+    encoder = WhisperEncoder(whisper_config)
+    print(f"Created WhisperEncoder: {encoder}")
+    return feature_extractor, encoder
 @click.command()
 @click.option("--audio_lm_path", type=str, required=True)
 @click.option("--llm_path", type=str, required=True)
@@ -15,8 +25,16 @@ import soundfile as sf
 @click.option("--audio_path", type=str, required=False,default="/home/yueyulin/github/RWKVTTS/my_chinese.wav")
 def generate_single_sample(audio_lm_path, llm_path, ckpt_path, language, device, whisper_path, audio_path):
     whisper_feature_extractor, whisper_encoder = load_whisper_feature_extractor_and_encoder(whisper_path)
-    audio_lm_model = RWKV7ModelForLatentInputs.from_pretrained(audio_lm_path, trust_remote_code=True)
-    llm = RWKV7ForCausalLM.from_pretrained(llm_path, trust_remote_code=True)
+    audio_lm_model_config = RWKV7Config.from_pretrained(audio_lm_path)
+    print(f"Loaded audio_lm_model_config: {audio_lm_model_config}")
+    with transformers.modeling_utils.no_init_weights():
+        audio_lm_model = RWKV7ModelForLatentInputs(audio_lm_model_config)
+    print(f"Loaded audio_lm_model: {audio_lm_model}")
+    llm_config = RWKV7Config.from_pretrained(llm_path)
+    print(f"Loaded llm_config: {llm_config}")
+    with transformers.modeling_utils.no_init_weights():
+        llm = RWKV7ForCausalLM(llm_config)
+    print(f"Loaded llm: {llm}")
     asr_model = RWKV7ASRModel(whisper_encoder,audio_lm_model, llm, whisper_feature_extractor)
     asr_model.eval()
     info = asr_model.load_state_dict(torch.load(ckpt_path),strict=False)
